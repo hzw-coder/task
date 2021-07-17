b26eeff9652374675d00ffeb312ae296485dd7df
hzw-coder <283401795@qq.com>
2021-07-17 10:00:16 +0800

    标签列表

--- src/pages/List/Label.vue
+++ src/pages/List/Label.vue
@@ -74,51 +74,8 @@ export default {
 
   data() {
     return {
-      options: [
-        {
-          value: "选项1",
-          label: "黄金糕",
-        },
-        {
-          value: "选项2",
-          label: "双皮奶",
-        },
-        {
-          value: "选项3",
-          label: "蚵仔煎",
-        },
-        {
-          value: "选项4",
-          label: "龙须面",
-        },
-        {
-          value: "选项5",
-          label: "北京烤鸭",
-        },
-      ],
-      value: "",
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄",
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄",
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄",
-        },
-      ],
+      // 表格数据
+      labelTableData: [],
       dialogVisible: false,
       addForm: {
         name: "",
@@ -128,8 +85,9 @@ export default {
       },
     };
   },
-
-  methods: {},
+  methods: {
+    getTableData,
+  },
 };
 </script>
 
